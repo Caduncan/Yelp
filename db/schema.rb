3cb65efbc90db128a1c07f1d2c7a6f4482a1d4a1
Chris A. Duncan <caduncan3@gmail.com>
2014-12-28 21:02:58 -0800

    added s3, figara, carrierwave images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229001302) do
+ActiveRecord::Schema.define(version: 20141229030226) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141229001302) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
